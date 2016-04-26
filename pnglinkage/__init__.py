87da54b8619a114a4787fd80dafecb70c915fad0
Antoine Lambert <antoine.lambert33@gmail.com>
2016-04-26 21:09:54 +0000

    fix cmake configuration and module imports on manylinux docker image

--- pnglinkage/__init__.py
+++ pnglinkage/__init__.py
@@ -1,4 +1,11 @@
+import sys
+import os.path
+
+sys.path.append(os.path.dirname(__file__))
+
 import _pnglinkage
+
+sys.path.pop()
 
 def processPngFile(pngFile):
     return _pnglinkage.processPngFile(pngFile)
--- zliblinkage/__init__.py
+++ zliblinkage/__init__.py
@@ -1,4 +1,11 @@
+import sys
+import os.path
+
+sys.path.append(os.path.dirname(__file__))
+
 import _zliblinkage
+
+sys.path.pop()
 
 def compressString(s):
     return _zliblinkage.compressString(s)
